22288a11cb47b936e7acf05fba02eafd02498666
Arayik <arayikh@energizeglobal.com>
2020-03-21 02:01:50 +0400

    first commit

---
+++ src/main/java/my/java/security/app/JavaSecurityAppApplication.java
@@ -0,0 +1,12 @@
+package my.java.security.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSecurityAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaSecurityAppApplication.class, args);
+  }
+}
---
+++ src/test/java/my/java/security/app/JavaSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my.java.security.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSecurityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
